d9e53560f98ec58b1a002fed674ff53aa8ea7faa
Richard Deloge <r.deloge@uni-alteri.com>
2016-08-13 00:46:23 +0200

    Prepare bundle

---
+++ src/Runner/CapabilityInterface.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author : Richard Déloge, richarddeloge@gmail.com, https://teknoo.software
+ * Date: 13/08/16
+ * Time: 00:23
+ */
---
+++ src/Task/TaskInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * East CodeRunnerBundle.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2016 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/east/coderunner Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+namespace Teknoo\East\CodeRunnerBundle\Task;
+
+use Teknoo\East\CodeRunnerBundle\Manager\TaskManagerInterface;
+
+interface TaskInterface
+{
+    public function setCode(CodeInterface $code): TaskInterface;
+
+    public function getCode(): CodeInterface;
+
+    public function getUrl(): string;
+
+    public function getStatus(): StatusInterface;
+
+    public function getResult(): ResultInterface;
+
+    public function registerTaskManager(string $taskUrl, TaskManagerInterface $taskManager): TaskInterface;
+
+    public function registerResult(TaskManagerInterface $taskManager): TaskInterface;
+}
