91b385754a12ff638ac2d5725432143e2d4f69f9
Richard Deloge <richarddeloge@gmail.com>
2016-08-19 23:14:35 +0200

    wrte business code

---
+++ src/Task/Task.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * East CodeRunnerBundle.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2016 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/east/coderunner Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+namespace Teknoo\East\CodeRunnerBundle\Task;
+
+use Teknoo\East\CodeRunnerBundle\Manager\TaskManagerInterface;
+
+class Task implements TaskInterface
+{
+    /**
+     * @var CodeInterface
+     */
+    private $code;
+
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * @var StatusInterface
+     */
+    private $status;
+
+    /**
+     * @var ResultInterface
+     */
+    private $result;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCode(CodeInterface $code): TaskInterface
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCode(): CodeInterface
+    {
+        return $this->code;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getStatus(): StatusInterface
+    {
+        return $this->status;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResult(): ResultInterface
+    {
+        return $this->result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerTaskManagerExecuting(string $taskUrl, TaskManagerInterface $taskManager): TaskInterface
+    {
+        $this->url = $taskUrl;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerResult(TaskManagerInterface $taskManager, ResultInterface $result): TaskInterface
+    {
+        $this->result = $result;
+
+        return $this;
+    }
+}
