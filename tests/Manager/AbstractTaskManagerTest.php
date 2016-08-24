eb6d71728cf406e59bdbe38f20ea9bcd6ec45418
Richard Deloge <richarddeloge@gmail.com>
2016-08-25 00:55:51 +0200

    Prepares states to clean object

--- tests/Manager/AbstractTaskManagerTest.php
+++ tests/Manager/AbstractTaskManagerTest.php
@@ -32,7 +32,7 @@ abstract class AbstractTaskManagerTest extends \PHPUnit_Framework_TestCase
      * To get an instance of the class to test
      * @return TaskManagerInterface
      */
-    abstract public function buildManager(): TaskManagerInterface
+    abstract public function buildManager(): TaskManagerInterface;
 
     public function testGetIdentifierReturn()
     {
