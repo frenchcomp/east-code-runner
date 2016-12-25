2b5cf90c51a7e7ce2c317a135f6f6c7caec3a9fa
Richard Deloge <richarddeloge@gmail.com>
2016-12-25 20:09:53 +0100

    write tests

---
+++ tests/symfony/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * East CodeRunner.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2016 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/east/coderunner Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+
+namespace Teknoo\Tests\East\CodeRunnerBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Teknoo\East\CodeRunnerBundle\DependencyInjection\Configuration;
+
+/**
+ * Class ConfigurationTest.
+ *
+ * @covers \Teknoo\East\CodeRunnerBundle\DependencyInjection\Configuration
+ */
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return Configuration
+     */
+    public function buildConfiguration()
+    {
+        return new Configuration();
+    }
+
+    public function testGetConfigTreeBuilder()
+    {
+        $configuration = $this->buildConfiguration();
+        $treeBuilder = $configuration->getConfigTreeBuilder();
+
+        $this->assertInstanceOf(TreeBuilder::class, $treeBuilder);
+    }
+}
